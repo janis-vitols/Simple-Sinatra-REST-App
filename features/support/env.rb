c67d7fed60f768acdb801157ce734b9e1930886f
Jānis Vītols <janis.vitols@icloud.com>
2015-09-18 22:48:25 +0300

    Configure cucumber `env` file
    - Require some gems which will be used in tests:
      * pry - in case of debugging;
      * rack/test - to use some great Rack::Test::Methods helper methods
      inside step definitions;
    
    - Prepare AppHelper module to return Class name in `#app` method
      (Rack-Test needs such);
    - Load Rack::Test::Methods and AppHelper in cucumber World.

---
+++ features/support/env.rb
@@ -0,0 +1,16 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'simple_rest_app')
+require 'pry'
+require 'rack/test'
+
+# SimpleRESTApp helper module
+# Rack-Test needs to know which Rack application it should talk to
+module AppHelper
+  # Rack-Test expects the app method to return application Class
+  def app
+    SimpleRESTApp
+  end
+end
+
+# To be able to use Rack-Test’s helper methods
+# (Rack::Test::Methods) HTTP simulation methods like get
+World(Rack::Test::Methods, AppHelper)
